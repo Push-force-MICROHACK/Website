b878f65011789929540e6f5fb04c65fe9e565f92
Ghalem Salim <salimghalem40@gmail.com>
2024-04-26 10:39:03 +0100

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
