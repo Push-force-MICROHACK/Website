6c49c7e6e30d86ca64281b9ccc3b4fcfb346a4e0
Ghalem Salim <salimghalem40@gmail.com>
2024-04-26 20:37:40 +0100

    20% ia part

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import Description from '../../Components/Description/Description'
-import Iframe from '../../Components/Iframe/Iframe'
-import HelpGuid from '../../Components/HelpGuide/HelpGuid'
-import WhyCongicore from '../../Components/WhyCognicore/WhyCognicore'
-import Features from '../../Components/Features/Features'
-import Subscription from '../../Components/Subscription/Subscription'
+import Description from "../../Components/Description/Description";
+import Iframe from "../../Components/Iframe/Iframe";
+import HelpGuid from "../../Components/HelpGuide/HelpGuid";
+import WhyCongicore from "../../Components/WhyCognicore/WhyCognicore";
+import Features from "../../Components/Features/Features";
+import Subscription from "../../Components/Subscription/Subscription";
 export default function Home() {
   return (
     <div>
-      <div className="space-sections"><Description/></div>
-      <div className="space-sections"> <Iframe/></div>
-      <div className="space-sections"><WhyCongicore/></div>
-      <div className="space-sections"> <Features/></div>
-      <div className="space-sections"><Subscription/></div>
-      <div className="space-sections"> <HelpGuid/></div>
+      <div className="space-sections">
+        <Description />
+      </div>
+      <div className="space-sections">
+        {" "}
+        <Iframe />
+      </div>
+      <div className="space-sections">
+        <WhyCongicore />
+      </div>
+      <div className="space-sections">
+        {" "}
+        <Features />
+      </div>
+      <div className="space-sections">
+        <Subscription />
+      </div>
+      <div className="space-sections">
+        {" "}
+        <HelpGuid />
+      </div>
     </div>
-  )
+  );
 }
