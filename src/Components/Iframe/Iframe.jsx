d9a9ad1ed5539b1f9511b431d82cd1ea1727d535
Ghalem Salim <salimghalem40@gmail.com>
2024-04-27 08:13:40 +0100

    salim's work

--- src/Components/Iframe/Iframe.jsx
+++ src/Components/Iframe/Iframe.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import "./Iframe.css"
-import picture from '../../assets/D3-2.svg'
+import React from "react";
+import "./Iframe.css";
+import picture from "../../assets/D3-2.svg";
 export default function Iframe() {
   return (
-    <div className='iframesection'>
+    <div className="iframesection">
       <iframe
-        width="560"
-        height="315"
         src="https://www.youtube.com/embed/SrdkyYHStbg?si=8YLVa2jIUTk1oUBC"
         title="YouTube Video"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       />
-      <div className='D3image2'>
-        <img src={picture} alt="3dlogo" className='img3D2' />
+      <div className="D3image2">
+        <img src={picture} alt="3dlogo" className="img3D2" />
       </div>
     </div>
   );
